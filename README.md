866fe1fa946642f29e250093d46ef05453e0dbdc
Ansgar Lichter <lichteransgar@gmail.com>
2020-11-17 22:31:56 +0100

    created autoamtion script, added todos

---
+++ README.md
@@ -0,0 +1,10 @@
+#Project Initialization Automation
+
+This project has the target to automatically execute the following steps:
+
+* Create windows directory at a given projects directory.
+* Create github repository with given credentials.
+* Initialize the local repository.
+* Add the origin to the repository.
+* Add initial readme with project name.
+* Push initial commit to the online repository.TTF
---
+++ create_dir.py
@@ -0,0 +1,43 @@
+import os
+from github import Github
+import git
+
+#TODO: read from config file
+repositoriesPath = r'C:\Users\Ansgar\Development\repositories'
+
+#TODO: get from console
+repositoryName = 'test'
+
+repositoryPath = repositoriesPath + '\\' + repositoryName
+if os.path.exists(repositoryPath):
+    print("Directory already existing")
+    quit()
+
+# Create Local Repository
+repositoryDirectory = os.path.join(repositoriesPath, repositoryName)
+repository = git.Repo.init(repositoryDirectory)
+
+readmePath = repositoryPath + "\\" + "README.md"
+readme = open( readmePath, "w+")
+readme.write("# " + repositoryName)
+readme.close()
+
+#TODO: read from config file
+user = Github("e231fd5675aeab7a1855f2b8dec8903c6f9adb08").get_user()
+gitRepository = user.create_repo(repositoryName)
+
+origin = repository.create_remote('origin', gitRepository.ssh_url)
+origin.fetch()
+
+
+repository.index.add("README.md")
+repository.index.commit("Created repository with an empty README")
+
+repository.git.branch("main")
+repository.git.checkout("main")
+repository.git.push("-u", "origin", "main")
+
+if user.get_repo(repositoryName):
+    print("Successfully created repository " + repositoryName + " with url " + gitRepository.ssh_url)
+else:
+    print("Repository not created")
